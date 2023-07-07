a690402aca443fa6459729662330bbade84dad02
Bai Qing Song <904494377@qq.com>
2023-07-07 17:42:49 +0800

    二维码的引用

--- app/src/main/java/com/dawn/libqrcode/MainActivity.java
+++ app/src/main/java/com/dawn/libqrcode/MainActivity.java
@@ -3,6 +3,9 @@ package com.dawn.libqrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.dawn.qr_code.LQrCodeUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((ImageView)findViewById(R.id.iv_logo)).setImageBitmap(LQrCodeUtil.createQRCodeBitmap("https://www.baidu.com", 400, 400));
     }
 }
